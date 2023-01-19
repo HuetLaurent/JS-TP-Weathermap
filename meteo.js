b4b0b5ac0411300fa490f10e553b3564d328569b
HuetLaurent <118887843+HuetLaurent@users.noreply.github.com>
2023-01-19 16:41:05 +0100

    Second commit

--- meteo.js
+++ meteo.js
@@ -1,8 +1,12 @@
 let API_KEY = "8d1ea57f5c7bb682b127c50f23b95b2b";
 
+let fleche = document.getElementById("divUne");
+fleche.hidden = true;
+
 let btnLocation = document.getElementById("btnLocation");
 
 btnLocation.onclick = function() {
+    fleche.hidden = false;
     let location = document.getElementById("location").value;
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`)
@@ -14,13 +18,52 @@ btnLocation.onclick = function() {
 
         let ville = data.name;
         document.getElementById("ville").innerHTML = ville;
+        if (ville.length <= 10) {
+            document.getElementById("ville").style.fontSize = "60px";
+        } else {
+            document.getElementById("ville").style.fontSize = "30px";
+        }
 
         let meteo1 = data.weather[0].main;
-        document.getElementById("meteo1").innerHTML = meteo1;
-
+        document.getElementById("meteo1").innerHTML = `Weather : ${meteo1}`;
+            
         let meteo2 = data.weather[0].description;
         document.getElementById("meteo2").innerHTML = meteo2;
-        }
+
+        let wind = "Wind :";
+        document.getElementById("wind").innerHTML = wind;
+
+        let deg = data.wind.deg;
+        document.getElementById("deg").innerHTML = `${deg}°`;
+
+        let speed = data.wind.speed;
+        document.getElementById("speed").innerHTML = `${speed} Km/h`;
+
+        document.getElementById("arrow").style.transform = `rotate(${deg}deg)`;
+
+        //##################################################
         
+        let temperature1 = Math.round(data.main.temp - 273.15);
+        document.getElementById("temperature1").innerHTML = `${temperature1}C°`;
+
+        let temperature2 = data.main.temp;
+        document.getElementById("temperature2").innerHTML = `${temperature2} Kelvin`;
+
+        let coordinate = "coordinate :";
+        document.getElementById("coordinate").innerHTML = coordinate;
+
+        let coordinate1 = data.coord.lat;
+        document.getElementById("coordinate1").innerHTML = coordinate1;
+
+        let coordinate2 = data.coord.lon;
+        document.getElementById("coordinate2").innerHTML = coordinate2;
+
+        let timeZ = "Time zone: "
+        document.getElementById("timeZ").innerHTML = timeZ;
+
+        let timeZone = data.timezone;
+        document.getElementById("timeZone").innerHTML = timeZone;
+        }
+  
     )
 }
